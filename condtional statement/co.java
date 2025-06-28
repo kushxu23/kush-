67d106f63c71ee55b979c54b78a64fa9a0e575dd
kushxu23 <kushxu@gmail.com>
2025-06-29 00:00:23 +0530

    https://github.com/kushxu23/kush-.git

---
+++ condtional statement/co.java
@@ -0,0 +1,14 @@
+ import java.util.*;
+  public class co {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        int A = sc.nextInt() ;
+        if(A>0) {
+            System.out.println("postive number ") ;
+        }
+        else{
+            System.out.println("negtive number ") ;
+        }
+    
+    }
+  }
