47aa41a9cd6340f5485861bd7842691c9e7246e6
kushxu23 <kushxu@gmail.com>
2025-06-29 00:53:26 +0530

    https://github.com/kushxu23/kush-.git

--- condtional statement/ko.java
+++ condtional statement/ko.java
@@ -12,7 +12,7 @@
         }*/ 
 //QUESTION 2
         
-        Scanner sc = new Scanner(System.in);
+     /*x  Scanner sc = new Scanner(System.in);
         double temp = sc.nextDouble() ;
 
         if (temp>100) {
@@ -21,10 +21,42 @@
         }
         else {
             System.out.println("NO fever") ;
+
+        } */
+
+  //qUESTION 3 PROGRAM to input week number and print the day of week name using swtich case 
+        Scanner sc = new Scanner(System.in );
+        int WEEK = sc.nextInt();
+        switch(WEEK) {
+            case 1 :
+            System.out.println("MONDAY") ;
+             break ;
+            case 2 :
+             System.out.println("TUESDAY") ;
+              break ;
+            case 3 :
+            System.out.println("WEDNESDAY") ;
+             break ;
+            case 4 :
+            System.out.println("thurdsay") ;
+             break ;
+            case 5 :
+            System.out.println("friday") ;
+             break ;
+            case 6 :
+            System.out.println("saturday") ;
+              break ;
+            case 7 :
+             System.out.println("sunday") ;
+             break ;
+             default :
+             System.out.println("not your day") ;
+
+
+             
             
         }
-
-
+ 
     
            
  
