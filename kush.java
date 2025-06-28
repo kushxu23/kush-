51fd2a3ef0712011ecf303f7ba2f8afabd96ec9a
kushxu23 <kushxu@gmail.com>
2025-06-28 17:42:52 +0530

    calcultor

--- kush.java
+++ kush.java
@@ -1,6 +1,6 @@
 import java.util.*;
  public class kush {
-  public static void main(String [] args)  {
+  public static void main(String[] args)  {
     Scanner sc = new Scanner(System.in) ;
     int A = sc.nextInt();
     int B = sc.nextInt();
