1ca262a1017a0e82506ee19b3ce2af1d43da8c92
kushxu23 <kushxu@gmail.com>
2025-06-29 00:10:06 +0530

    https://github.com/kushxu23/kush-.git

--- condtional statement/ko.java
+++ condtional statement/ko.java
@@ -1,5 +1,6 @@
  import java.util.*;
-  public class co {
+  public class ko  {
+  
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in) ;
         int A = sc.nextInt() ;
