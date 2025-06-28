be5488306204a8433bfe21b1fc244c1926e872e4
kushxu23 <kushxu@gmail.com>
2025-06-29 00:33:03 +0530

    https://github.com/kushxu23/kush-.git

--- condtional statement/ko.java
+++ condtional statement/ko.java
@@ -2,14 +2,31 @@
   public class ko  {
   
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in) ;
+       /* Scanner sc = new Scanner(System.in) ;
         int A = sc.nextInt() ;
         if(A>0) {
             System.out.println("postive number ") ;
         }
         else{
             System.out.println("negtive number ") ;
+        }*/ 
+//QUESTION 2
+        
+        Scanner sc = new Scanner(System.in);
+        double temp = sc.nextDouble() ;
+
+        if (temp>100) {
+            System.out.println("HIGH FEVEER") ;
+
         }
+        else {
+            System.out.println("NO fever") ;
+            
+        }
+
+
     
+           
+ 
     }
   }
