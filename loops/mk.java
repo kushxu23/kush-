747cd0b0d9d1272aeb5778fbbfd930c797677c67
kushxu23 <kushxu@gmail.com>
2025-07-03 08:36:57 +0530

    loop

---
+++ loops/mk.java
@@ -0,0 +1,18 @@
+package loops;
+
+import java.util.*;
+public class mk {
+ public static void main(String[] arg)  {
+    //print number from 1 to 10
+    int counter = 1;
+    while(counter <=10) {
+        System.out.println(counter) ;
+        counter++ ;
+    }
+    
+
+     
+    
+ }
+}
+  
